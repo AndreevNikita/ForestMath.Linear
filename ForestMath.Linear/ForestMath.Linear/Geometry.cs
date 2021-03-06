99a3deb8c83eb53bc6b3e7cb27ee5a286179002d
Nikita Andreev <nickandreevart@gmail.com>
2021-03-06 16:41:36 +0300

    Ray rotation fix

--- ForestMath.Linear/ForestMath.Linear/Geometry.cs
+++ ForestMath.Linear/ForestMath.Linear/Geometry.cs
@@ -123,11 +123,11 @@ namespace ForestMath.Linear {
 		}
 
 		public static Ray3 RotateFull(this Ray3 ray, Quaternion quaternion) {
-			return Ray3.FromTwoPoints(Rotate(ray.StartPoint, quaternion), Rotate(ray.StartPoint + ray.Dir, quaternion), ray.IsLine);
+			return Ray3.FromTwoPoints(ray.StartPoint.Rotate(quaternion), ray.Dir.Rotate(quaternion), ray.IsLine);
 		}
 
 		public static Ray3 RotateDir(this Ray3 ray, Quaternion quaternion) {
-			return new Ray3(ray.StartPoint, Rotate(ray.StartPoint + ray.Dir, quaternion), ray.IsLine);
+			return new Ray3(ray.StartPoint, ray.Dir.Rotate(quaternion), ray.IsLine);
 		}
 
 		//Shifts
